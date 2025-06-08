4958ba6f0fa6526dbb4d37bea23e0f7a41305425
Mario Càllisto <mario.callisto@gmail.com>
2025-06-08 16:52:20 +0200

    Improved outliers test

--- src/test/scala/io/github/scala_tessella/tessella/OutliersToSVGFileSpec.scala
+++ src/test/scala/io/github/scala_tessella/tessella/OutliersToSVGFileSpec.scala
@@ -1,7 +1,6 @@
 package io.github.scala_tessella.tessella
 
 import io.github.scala_tessella.tessella.conversion.SVG.toSVG
-
 import org.scalatest.*
 import org.scalatest.flatspec.*
 import org.scalatest.matchers.*
@@ -11,21 +10,8 @@ class OutliersToSVGFileSpec extends AnyFlatSpec with Helper with should.Matchers
   val allOutliers: List[Tiling] =
     Outliers.all
 
-  "All outliers" must "be valid" in {
-    allOutliers.forall(tiling =>
-      try {
-        val edges = tiling.graphEdges
-        println(edges.size)
-        true
-      } catch {
-        case _: Throwable => false
-      }
-    ) shouldBe true
-  }
-
-//  they must "be saved as .SVG files to a dir" in {
+  "All outliers" must "be saved as .SVG files to a dir" in {
 //    Outliers.all.indices.foreach(index =>
-//      println(index)
 //      try {
 //        val tiling: Tiling = Outliers.all(index)
 //        saveFileSVG(tiling.toSVG(), s"test/$index")
@@ -33,4 +19,4 @@ class OutliersToSVGFileSpec extends AnyFlatSpec with Helper with should.Matchers
 //        case e: Throwable => println(s"Error for $index: $e")
 //      }
 //    )
-//  }
+  }
